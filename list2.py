42ce04f41709ed41b940305238059d5b35840cca
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:57:16 -0800

    lists 2

--- list2.py
+++ list2.py
@@ -5,3 +5,9 @@ print list1[5]
 print list1[5][1]  # prints list at 5 and index 2
 # strings are implimented as lists thus 
 print list1[1][1]  # prints list1 index 1, sub index 1
+
+#range(0,10) = [0,1,2,......9]
+for i in list1:
+    print i
+
+print range(0,10)
