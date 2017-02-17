58cfba821529db71dc18f0e38ef580e8fe52fa18
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:27:08 -0800

    functions

---
+++ funtions.py
@@ -0,0 +1,12 @@
+# Convert C to F 
+
+def celToF(tempurature):
+    f = tempurature*9/5 + 32
+    print f
+    return f 
+
+faren = celToF(12) #caller of the function
+print "test"
+print faren
+
+print celToF(13)
