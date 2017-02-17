eadaacabafab5a69171f87f955a9779f6364bb91
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 19:26:52 -0800

    Data Types strings

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 # Data Types
-#integer : 1,2,3
+
+# integer : 1,2,3
 # float : 1.0, 2.0
 # boolean : True False
-
 num = True
 print num, type(num)
+# string "name" 'name' Single and 
+# double are the same
+str1 = 'Name'
+print str1, type(str1)
