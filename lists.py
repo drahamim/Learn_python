34c50dd83ecd0a29ba9cadcfa70e5b11657f0e3d
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:37:21 -0800

    lists

--- lists.py
+++ lists.py
@@ -1,5 +1,11 @@
 #list: is a collection of "items"
 
-list1 = [1, 2, 3, 4]
+list1 = [100, 21, 32, 4]
+#        0    1   2   3
 print list1
-print type(list1)
+print type(list1) 
+print list1[2]  # print specific element
+
+print list1[0], list1[1]
+print list1[0:2]   # prints range index 0 to -1
+
