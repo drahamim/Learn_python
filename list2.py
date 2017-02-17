9033cbd15003cf29b1597bbc39e0d65169ed249a
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:55:13 -0800

    lists 2

--- list2.py
+++ list2.py
@@ -3,4 +3,5 @@ print list1[1], type(list1[1])
 print list1[4]
 print list1[5]
 print list1[5][1]  # prints list at 5 and index 2
+# strings are implimented as lists thus 
 print list1[1][1]  # prints list1 index 1, sub index 1
