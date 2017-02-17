a872cf41bf625bcdcfcf6d731ee837e720b13f2e
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:29:39 -0800

    functions tuples

--- funtions.py
+++ funtions.py
@@ -3,10 +3,13 @@
 def celToF(tempurature):
     f = tempurature*9/5 + 32
     print f
-    return f 
+    return f, tempurature
 
 faren = celToF(12) #caller of the function
 print "test"
 print faren
 
 print celToF(13)
+
+x,y = celToF(14)
+print x,y
