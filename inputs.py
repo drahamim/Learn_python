43d2575fee48439f10a0f85e56bb56b6db38b015
Daniel Rahamim <drahamim@LA-drahamim.local>
2017-02-16 20:21:48 -0800

    functions

--- inputs.py
+++ inputs.py
@@ -1,9 +1,10 @@
 # input 
 num = raw_input("enter a number: ")
-print int(num) * 10
-print float(num) *10
-'''
-block of comment
-comment 
-comment 
-'''
+# print int(num) * 10
+# print float(num) *10
+# '''
+# block of comment
+# comment 
+# comment 
+# '''
+
